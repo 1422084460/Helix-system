147ab828254a5f9dc9ecf842e5679ecd047a238f
lourong <1422084460@qq.com>
2022-01-13 10:41:25 +0800

    修改User和AppStart

--- art-common/src/main/java/com/art/artcommon/entity/User.java
+++ art-common/src/main/java/com/art/artcommon/entity/User.java
@@ -2,10 +2,17 @@ package com.art.artcommon.entity;
 
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.math.BigDecimal;
 
 @Data
 @TableName("User")
+@NoArgsConstructor
+@Accessors(chain = true)
 public class User {
     @TableField
     private int id;
@@ -15,4 +22,16 @@ public class User {
     private String password;
     @TableField
     private String avatar;
+    @TableField
+    private BigDecimal money;
+    @TableField
+    private boolean is_avatar_prepare;
+    @TableField
+    private String status;
+    @TableField
+    private String is_admin;
+    @TableField
+    private int score;
+    @TableField
+    private String phone_num;
 }
--- art-web/src/main/java/com/art/artweb/AppStart.java
+++ art-web/src/main/java/com/art/artweb/AppStart.java
@@ -1,17 +1,38 @@
 package com.art.artweb;
 
 import com.art.artcommon.entity.Store;
+import com.art.artcommon.utils.RedisUtil;
+import com.art.artservice.service.TestService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Component
 @Slf4j
 public class AppStart implements ApplicationListener<ApplicationStartedEvent> {
+
+    @Autowired
+    private TestService service;
+
     @Override
     public void onApplicationEvent(ApplicationStartedEvent event) {
         initStore();
+        initRedis();
+    }
+
+    private void initRedis() {
+        if (!RedisUtil.hasKey("isInitAlready")){
+            Map<String,String> map = new HashMap<>();
+            String email = service.getUser().getEmail();
+            map.put("test",email);
+            RedisUtil.pipLine(map,null);
+        }
+        log.info("redis数据初始化完成......");
     }
 
     /**
