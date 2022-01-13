f15ae743c386511d4fbde3eee1d9f9a179c8ceb1
lourong <1422084460@qq.com>
2022-01-13 10:55:42 +0800

    修改User

--- art-common/src/main/java/com/art/artcommon/entity/User.java
+++ art-common/src/main/java/com/art/artcommon/entity/User.java
@@ -2,17 +2,16 @@ package com.art.artcommon.entity;
 
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.experimental.Accessors;
 
 import java.math.BigDecimal;
 
 @Data
 @TableName("User")
 @NoArgsConstructor
-@Accessors(chain = true)
+@Builder
 public class User {
     @TableField
     private int id;
