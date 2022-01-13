03868dae1392afe9ce0b7a00de13089ef19dc7ba
lourong <1422084460@qq.com>
2022-01-13 10:41:25 +0800

    修改User和AppStart
    (cherry picked from commit 147ab828254a5f9dc9ecf842e5679ecd047a238f)

--- art-common/src/main/java/com/art/artcommon/entity/User.java
+++ art-common/src/main/java/com/art/artcommon/entity/User.java
@@ -2,14 +2,17 @@ package com.art.artcommon.entity;
 
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
-import lombok.Builder;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 
 import java.math.BigDecimal;
 
 @Data
 @TableName("User")
-@Builder
+@NoArgsConstructor
+@Accessors(chain = true)
 public class User {
     @TableField
     private int id;
