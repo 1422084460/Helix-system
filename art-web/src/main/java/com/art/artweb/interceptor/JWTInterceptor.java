acf8a15891944cc56046e5fb096a380b7dbd260c
lourong <1422084460@qq.com>
2022-02-16 15:20:42 +0800

    修改拦截器并添加全局异常处理类

--- art-web/src/main/java/com/art/artweb/interceptor/JWTInterceptor.java
+++ art-web/src/main/java/com/art/artweb/interceptor/JWTInterceptor.java
@@ -1,25 +1,21 @@
 package com.art.artweb.interceptor;
 
-import com.alibaba.fastjson.JSONObject;
 import com.art.artcommon.constant.R;
 import com.art.artcommon.entity.IResult;
 import com.art.artcommon.entity.Store;
 import com.art.artcommon.utils.JWTUtils;
-import com.auth0.jwt.exceptions.AlgorithmMismatchException;
-import com.auth0.jwt.exceptions.SignatureVerificationException;
 import com.auth0.jwt.exceptions.TokenExpiredException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Slf4j
 public class JWTInterceptor extends HandlerInterceptorAdapter {
     //拦截器于过滤器后执行
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws IOException {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception{
 
         String token = request.getHeader("token");
         IResult res;
@@ -29,9 +25,6 @@ public class JWTInterceptor extends HandlerInterceptorAdapter {
             res = IResult.success(null);
             Store.getInstance().put(name,Store.getInstance().MainDataPut("token验证", res));
             return true;
-        }catch (SignatureVerificationException e){
-            log.error("用户"+name+"访问："+request.getRequestURI()+"接口异常===>>"+e.getMessage());
-            res = IResult.fail(null,R.MSG_TOKEN_SIGNATURE_INVALID,R.CODE_TOKEN_SIGNATURE_INVALID);
         }catch (TokenExpiredException e){
             log.error("用户"+name+"访问："+request.getRequestURI()+"接口异常===>>"+e.getMessage());
             res = IResult.fail(null,R.MSG_TOKEN_EXPIRE,R.CODE_TOKEN_EXPIRE);
@@ -39,22 +32,7 @@ public class JWTInterceptor extends HandlerInterceptorAdapter {
                 Store.getInstance().put(name,Store.getInstance().MainDataPut("token验证", res));
                 return true;
             }
-        }catch (AlgorithmMismatchException e){
-            log.error("用户"+name+"访问："+request.getRequestURI()+"接口异常===>>"+e.getMessage());
-            res = IResult.fail(null,R.MSG_TOKEN_ALGORITHM_MISMATCH,R.CODE_TOKEN_ALGORITHM_MISMATCH);
-        }catch (Exception e) {
-            log.error("用户"+name+"访问："+request.getRequestURI()+"接口异常===>>"+e.getMessage());
-            res = IResult.fail(null,R.MSG_TOKEN_INVALID,R.CODE_TOKEN_INVALID);
+            throw new TokenExpiredException(R.MSG_TOKEN_EXPIRE);
         }
-        System.out.println("进入拦截器"+token);
-        falseResult(response,res);
-        return false;
-    }
-
-    public void falseResult(HttpServletResponse response,IResult res) throws IOException {
-        response.setCharacterEncoding("UTF-8");
-        response.setContentType("application/json; charset=utf-8");
-        Object json = JSONObject.toJSON(res);
-        response.getWriter().println(json);
     }
 }
