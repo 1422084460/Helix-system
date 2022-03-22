2d5931be53fa0926fc76d55ce6f759876930bb37
楼荣 <1422084460@qq.com>
2022-03-22 10:23:54 +0800

    添加异步处理模块

---
+++ art-web/src/main/java/com/art/artweb/config/AsyncTaskExecutePool.java
@@ -0,0 +1,50 @@
+package com.art.artweb.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.lang.reflect.Method;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * description
+ * 修改 springboot 默认线程池
+ * @author lou
+ * @create 2022/3/22
+ */
+@Configuration
+@Slf4j
+public class AsyncTaskExecutePool implements AsyncConfigurer {
+
+    @Autowired
+    private TaskThreadPoolConfig config;
+
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(config.getCorePoolSize());
+        executor.setMaxPoolSize(config.getMaxPoolSize());
+        executor.setKeepAliveSeconds(config.getKeepAliveSeconds());
+        executor.setQueueCapacity(config.getQueueCapacity());
+        executor.setThreadNamePrefix("default-ThreadPool-changer-");
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        executor.initialize();
+        return executor;
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return new AsyncUncaughtExceptionHandler() {
+            @Override
+            public void handleUncaughtException(Throwable ex, Method method, Object... params) {
+                log.error("exception info:" + ex.getMessage(),ex);
+                log.error("exception method:" + method.getName());
+            }
+        };
+    }
+}
---
+++ art-web/src/main/java/com/art/artweb/config/TaskThreadPoolConfig.java
@@ -0,0 +1,50 @@
+package com.art.artweb.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * description
+ *
+ * @author lou
+ * @create 2022/3/21
+ */
+@ConfigurationProperties(prefix = "task.pool")
+public class TaskThreadPoolConfig {
+
+    private int corePoolSize;
+    private int maxPoolSize;
+    private int keepAliveSeconds;
+    private int queueCapacity;
+
+    public int getCorePoolSize() {
+        return corePoolSize;
+    }
+
+    public int getMaxPoolSize() {
+        return maxPoolSize;
+    }
+
+    public int getKeepAliveSeconds() {
+        return keepAliveSeconds;
+    }
+
+    public int getQueueCapacity() {
+        return queueCapacity;
+    }
+
+    public void setCorePoolSize(int corePoolSize) {
+        this.corePoolSize = corePoolSize;
+    }
+
+    public void setMaxPoolSize(int maxPoolSize) {
+        this.maxPoolSize = maxPoolSize;
+    }
+
+    public void setKeepAliveSeconds(int keepAliveSeconds) {
+        this.keepAliveSeconds = keepAliveSeconds;
+    }
+
+    public void setQueueCapacity(int queueCapacity) {
+        this.queueCapacity = queueCapacity;
+    }
+}
