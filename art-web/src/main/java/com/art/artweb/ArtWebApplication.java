931d0bb81d19fcead4e7f6b60f6393e5e9d2089d
楼荣 <1422084460@qq.com>
2022-03-22 10:25:32 +0800

    启动类开启异步

--- art-web/src/main/java/com/art/artweb/ArtWebApplication.java
+++ art-web/src/main/java/com/art/artweb/ArtWebApplication.java
@@ -1,12 +1,17 @@
 package com.art.artweb;
 
+import com.art.artweb.config.TaskThreadPoolConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
+@EnableConfigurationProperties({TaskThreadPoolConfig.class})
 @ComponentScan(basePackages = {"com.art"})
 @EnableAspectJAutoProxy(exposeProxy = true)
 @MapperScan(basePackages = {"com.art.artcommon.mapper"})
