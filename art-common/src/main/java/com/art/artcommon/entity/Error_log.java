40ecf3485b4cbe5c84c0ea6df3541985f1e21129
楼荣 <1422084460@qq.com>
2022-03-24 15:08:13 +0800

    添加Errorlog实体类

--- art-common/src/main/java/com/art/artcommon/entity/Error_log.java
+++ art-common/src/main/java/com/art/artcommon/entity/Error_log.java
@@ -1,10 +1,33 @@
 package com.art.artcommon.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
 /**
  * description
  *
  * @author lou
  * @create 2022/3/24
  */
+@Data
+@TableName("Error_Log")
+@Accessors(chain = true)
 public class Error_log {
+
+    @TableId(type = IdType.AUTO)
+    private Integer id;
+    @TableField
+    private String error_msg;
+    @TableField
+    private String error_code;
+    @TableField
+    private String interface_path;
+    @TableField
+    private String caller;
+    @TableField
+    private long timestamp;
 }
--- art-common/src/main/java/com/art/artcommon/mapper/Error_logMapper.java
+++ art-common/src/main/java/com/art/artcommon/mapper/Error_logMapper.java
@@ -1,10 +1,13 @@
 package com.art.artcommon.mapper;
 
+import com.art.artcommon.entity.Error_log;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
 /**
  * description
  *
  * @author lou
  * @create 2022/3/24
  */
-public interface Error_logMapper {
+public interface Error_logMapper extends BaseMapper<Error_log> {
 }
