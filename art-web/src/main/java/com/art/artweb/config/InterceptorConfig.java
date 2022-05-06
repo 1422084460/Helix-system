82ded987d3beac3fd66545405d60a1fae4bf13a8
楼荣 <1422084460@qq.com>
2022-05-06 11:50:59 +0800

    修改拦截配置为以xml配置为前提

--- art-web/src/main/java/com/art/artweb/config/InterceptorConfig.java
+++ art-web/src/main/java/com/art/artweb/config/InterceptorConfig.java
@@ -1,17 +1,32 @@
 package com.art.artweb.config;
 
 import com.art.artweb.interceptor.JWTInterceptor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Slf4j
 @Configuration
 public class InterceptorConfig implements WebMvcConfigurer {
 
+    @Value("${dev-pattern.devInfo}")
+    private String mode;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new JWTInterceptor())
-                .addPathPatterns("/api/**")
-                .excludePathPatterns("/api/user/register");
+        log.info("以{}环境启动...",mode);
+        if (!mode.equals("debug")) {
+            registry.addInterceptor(new JWTInterceptor())
+                    .addPathPatterns("/api/**")
+                    .excludePathPatterns("/api/user/register")
+                    .excludePathPatterns("/api/user/sendCode")
+                    .excludePathPatterns("/api/user/verifyCode");
+        }else {
+            System.out.println("========测试环境");
+            registry.addInterceptor(new JWTInterceptor())
+                    .excludePathPatterns("/api/**");
+        }
     }
 }
