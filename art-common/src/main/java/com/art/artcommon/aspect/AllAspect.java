9db3a482e4d57d027930895ae2dfd52201181757
楼荣 <1422084460@qq.com>
2022-05-09 17:04:14 +0800

    更新测试切面类

--- art-common/src/main/java/com/art/artcommon/aspect/AllAspect.java
+++ art-common/src/main/java/com/art/artcommon/aspect/AllAspect.java
@@ -1,10 +1,16 @@
 package com.art.artcommon.aspect;
 
+import com.art.artcommon.custominterface.TaskExe;
+import com.art.artcommon.utils.AopTargetUtils;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
 
 /**
  * description
@@ -15,14 +21,30 @@ import org.springframework.stereotype.Component;
 @Component
 @Aspect
 public class AllAspect {
+    //考虑用around环绕建立锁，前置解锁，后置上锁
 
     @Pointcut("@annotation(com.art.artcommon.custominterface.Error)")
     public void cutError(){}
+    @Pointcut("@annotation(com.art.artcommon.custominterface.TaskExe)")
+    public void cutTaskExe(){}
 
-    @Around("cutError()")
-    public void around(ProceedingJoinPoint joinPoint) throws Throwable {
-        System.out.println("before");
-        joinPoint.proceed();
-        System.out.println("after");
+    @Before("cutError()")
+    public void before(){
+        System.out.println();
+    }
+
+    @After("cutTaskExe()")
+    public void after(JoinPoint point) throws Exception {
+        Signature s = point.getSignature();
+        MethodSignature m = (MethodSignature)s;
+        Object target = point.getTarget();
+        Method method = target.getClass().getMethod(m.getName(),m.getParameterTypes());
+        TaskExe taskExe = method.getAnnotation(TaskExe.class);
+        String name = taskExe.name();
+        System.out.println("后置通知"+name);
+    }
+
+    private String handlerError(HttpServletRequest req){
+        return req.getRequestURI();
     }
 }
