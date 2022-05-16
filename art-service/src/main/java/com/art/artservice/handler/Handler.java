4d0e4ddd32d3088daae3467670e83153b9e93ab3
楼荣 <1422084460@qq.com>
2022-05-16 11:12:37 +0800

    修改handler接口

--- art-service/src/main/java/com/art/artservice/handler/Handler.java
+++ art-service/src/main/java/com/art/artservice/handler/Handler.java
@@ -1,9 +1,17 @@
 package com.art.artservice.handler;
 
-import org.springframework.stereotype.Component;
-
-//@Component
+/**
+ * description
+ * 消息队列统一接口
+ * @author lou
+ * @create 2022/4/11
+ */
 public interface Handler {
 
+    /**
+     * 消息处理
+     * @param queue 队列名
+     * @param JsonEntity 消息
+     */
     void handler(String queue, String JsonEntity);
 }
