026f957671f17dbc8f713b13952ba695a53503bb
楼荣 <1422084460@qq.com>
2022-05-17 10:20:40 +0800

    删除项目重构多余的文件

--- art-common/src/main/java/com/art/artcommon/mapper/UserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.art.artcommon.mapper;
-
-import com.art.artcommon.entity.User;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-
-public interface UserMapper extends BaseMapper<User> {
-}
