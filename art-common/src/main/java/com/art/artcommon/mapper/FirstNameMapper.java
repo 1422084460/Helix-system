0da80616fcd15be327c544fe8736fd81e40fcb88
楼荣 <1422084460@qq.com>
2022-05-17 11:23:37 +0800

    删除项目重构后多余文件

--- art-common/src/main/java/com/art/artcommon/mapper/FirstNameMapper.java
+++
@@ -1,7 +0,0 @@
-package com.art.artcommon.mapper;
-
-import com.art.artcommon.entity.FirstName;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-
-public interface FirstNameMapper extends BaseMapper<FirstName> {
-}
--- art-common/src/main/java/com/art/artcommon/mapper/LastNameMapper.java
+++
@@ -1,7 +0,0 @@
-package com.art.artcommon.mapper;
-
-import com.art.artcommon.entity.LastName;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-
-public interface LastNameMapper extends BaseMapper<LastName> {
-}
