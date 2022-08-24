ff1e31687e5c1c964e22ff366e1e962a82748e9e
楼荣 <1422084460@qq.com>
2022-08-24 11:10:13 +0800

    test

--- art-web/src/main/java/com/art/artweb/controller/TestController.java
+++ art-web/src/main/java/com/art/artweb/controller/TestController.java
@@ -2,15 +2,29 @@ package com.art.artweb.controller;
 
 //import com.art.artcommon.mapper.WatcherMapper;
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.art.artadmin.entity.User;
+import com.art.artadmin.mapper.UserMapper;
+import com.art.artcommon.custominterface.CachedTable;
+import com.art.artcommon.custominterface.Error;
 import com.art.artcommon.entity.IResult;
 import com.art.artcommon.entity.Store;
+import com.art.artcommon.utils.DBUtils;
 import com.art.artcommon.utils.JWTUtils;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 
 @RestController
@@ -80,4 +94,63 @@ public class TestController {
 //        }
 //    }
 
+    @Autowired
+    private DBUtils utils;
+
+    @RequestMapping("/testSql")
+    @CachedTable(key = "test",tableName = "CacheTables")
+    public IResult testSql(@RequestBody JSONObject data){
+        log.info("开始测试");
+//        try {
+//            TimeUnit.SECONDS.sleep(60);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+        log.info("结束测试");
+        return IResult.success();
+    }
+
+    @Autowired
+    private UserMapper userMapper;
+    @Autowired
+    private DataSource dataSource;
+
+    @RequestMapping("/testDruid")
+    public void testDruid() throws SQLException {
+        System.out.println(dataSource);
+        System.out.println(dataSource.getConnection());
+        System.out.println(dataSource);
+    }
+
+    @RequestMapping("/testAnno")
+    @Error
+    public IResult testAnno(){
+        log.info(Thread.currentThread().getName());
+        return IResult.success();
+    }
+
+    private volatile Semaphore semaphore = new Semaphore(3);
+
+    @RequestMapping("/testThread")
+    public IResult testThread(){
+        CompletableFuture<Void> future = CompletableFuture.runAsync(()->{
+            while (true){
+                try {
+                    if (semaphore.tryAcquire()) {
+                        log.info("go in...");
+                        TimeUnit.SECONDS.sleep(6);
+                        semaphore.release();
+                        break;
+                    }else {
+                        log.info("wait...");
+                        TimeUnit.SECONDS.sleep(3);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            log.info("go out...");
+        });
+        return IResult.success();
+    }
 }
