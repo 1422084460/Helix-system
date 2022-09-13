ccb4e5a4dda323f8a39d14849aafb283291dd529
楼荣 <1422084460@qq.com>
2022-09-13 15:05:20 +0800

    add后台管理方法

--- art-admin/src/main/java/com/art/artadmin/service/ManageService.java
+++ art-admin/src/main/java/com/art/artadmin/service/ManageService.java
@@ -1,6 +1,19 @@
 package com.art.artadmin.service;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.TypeReference;
+import com.art.artadmin.entity.PersonalizedDress;
+import com.art.artadmin.mapper.PersonalizedDressMapper;
+import com.art.artcommon.utils.RedisUtil;
+import com.art.artcommon.utils.Tools;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 /**
  * description
@@ -11,5 +24,57 @@ import org.springframework.stereotype.Service;
 @Service
 public class ManageService {
 
+    @Autowired
+    private PersonalizedDressMapper personalizedDressMapper;
+
     public void updateModule(){}
+
+    /**
+     * 添加个性装扮
+     * @param data 请求数据
+     * @return int
+     */
+    public int addPersonalizedDress(JSONObject data){
+        long timestamp = data.getLongValue("timestamp");
+        String itemName = data.getString("itemName");
+        String itemUrl = data.getString("itemUrl");
+        int itemPrice = data.getIntValue("itemPrice");
+        String itemCategory = data.getString("itemCategory");
+        int overdue = data.getIntValue("overdue");
+        String itemId = Tools.getCode() + Tools.dateToStr(timestamp).substring(0,8);
+        PersonalizedDress entity = new PersonalizedDress()
+                .setItem_id(itemId)
+                .setItem_name(itemName)
+                .setItem_url(itemUrl)
+                .setItem_category(itemCategory)
+                .setItem_price(itemPrice)
+                .setOverdue(overdue)
+                .setIs_available(1);
+        return personalizedDressMapper.insert(entity);
+    }
+
+    /**
+     * 展示个性装扮
+     * @param data 请求数据
+     * @return List<PersonalizedDress>
+     */
+    public List<PersonalizedDress> showPersonalizedDress(JSONObject data){
+        List<PersonalizedDress> list = null;
+        if (!RedisUtil.hasKey("PersonalizedDressList")) {
+            list = personalizedDressMapper.selectList(null);
+            RedisUtil.set("PersonalizedDressList", JSON.toJSONString(list),12, TimeUnit.HOURS);
+        }
+        String category = data.getString("category");
+        int overdue = data.getIntValue("overdue");
+        int available = data.getIntValue("available");
+        if (list == null){
+            String s = RedisUtil.get("PersonalizedDressList");
+            list = JSON.parseObject(s,new TypeReference<List<PersonalizedDress>>(){});
+        }
+        return list.stream()
+                .filter(c -> category.equals("") || c.getItem_category().equals(category))
+                .filter(o -> overdue == 0 || o.getOverdue() == overdue)
+                .filter(a -> available == 0 || a.getIs_available() == available)
+                .collect(Collectors.toList());
+    }
 }
--- art-web/src/main/java/com/art/artweb/controller/ManageController.java
+++ art-web/src/main/java/com/art/artweb/controller/ManageController.java
@@ -1,12 +1,16 @@
 package com.art.artweb.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import com.art.artadmin.entity.PersonalizedDress;
 import com.art.artadmin.service.ManageService;
+import com.art.artcommon.constant.R;
 import com.art.artcommon.entity.IResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * description
@@ -21,15 +25,41 @@ public class ManageController {
     @Autowired
     private ManageService manageService;
 
+    /**
+     * 修改用户角色权限
+     * @param data 请求数据
+     * @return IResult
+     */
     @RequestMapping("/updateModule")
     public IResult updateModule(@RequestBody JSONObject data){
         manageService.updateModule();
         return IResult.success();
     }
 
+    /**
+     * 添加个性装扮
+     * @param data 请求数据
+     * @return IResult
+     */
     @RequestMapping("/addPersonalizedDress")
     public IResult addPersonalizedDress(@RequestBody JSONObject data){
-        manageService.updateModule();
-        return IResult.success();
+        int res = manageService.addPersonalizedDress(data);
+        if (res == 1){
+            return IResult.success();
+        }
+        return IResult.fail("添加失败", R.CODE_FAIL);
+    }
+
+    /**
+     * 展示个性装扮
+     * @param data 请求数据
+     * @return IResult
+     */
+    @RequestMapping("/showPersonalizedDress")
+    public IResult showPersonalizedDress(@RequestBody JSONObject data){
+        List<PersonalizedDress> res = manageService.showPersonalizedDress(data);
+        JSONObject object = new JSONObject();
+        object.put("PersonalizedDress",res);
+        return IResult.success(object);
     }
 }
