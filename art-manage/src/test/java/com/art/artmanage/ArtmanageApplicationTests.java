8a810fc333721969717dd22781db46a11baeb4b3
楼荣 <1422084460@qq.com>
2022-09-15 10:44:51 +0800

    add管理模块

---
+++ art-manage/src/test/java/com/art/artmanage/ArtmanageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.art.artmanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtmanageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
