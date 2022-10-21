a5503355cceeb2eef35c708acca533cc7b7e7e4f
楼荣 <1422084460@qq.com>
2022-10-21 12:10:42 +0800

    add文件下载功能

--- art-common/src/main/java/com/art/artcommon/utils/FileUtils.java
+++ art-common/src/main/java/com/art/artcommon/utils/FileUtils.java
@@ -1,93 +1,85 @@
 package com.art.artcommon.utils;
 
-import com.jcraft.jsch.Channel;
+import com.art.artcommon.factory.SftpPool;
 import com.jcraft.jsch.ChannelSftp;
-import com.jcraft.jsch.JSch;
-import com.jcraft.jsch.Session;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.io.OutputStream;
+
+/**
+ * description
+ * 文件工具类
+ * @author lou
+ * @create 2022/10/17
+ */
 
 @Slf4j
 @Component
 public class FileUtils {
 
-    @Value("${upload-file.data.port}")
-    private int port;
-    @Value("${upload-file.data.password}")
-    private String password;
-    @Value("${upload-file.data.ip}")
-    private String ip;
-    @Value("${upload-file.data.user}")
-    private String user;
-    @Value("${upload-file.data.filepath}")
+    @Autowired
+    private SftpPool pool;
+
+    @Value("${upload-file.data.avatarpath}")
     private String filepath;
 
-    public void upload(byte[] bytes,String fileName) throws Exception{
-        //int port = 22;
-        //String user = "root";
-        //String password = "HAPPY142208*.";
-        //String ip = "118.31.55.121";
-        // 服务器保存路径
-        //String filepath = "/usr/local/images/";
-        Session session = null;
-        Channel channel = null;
-
-        JSch jSch = new JSch();
-
-        if(port <=0){
-            //连接服务器，采用默认端口
-            session = jSch.getSession(user, ip);
-        }else{
-            //采用指定的端口连接服务器
-            session = jSch.getSession(user, ip ,port);
-        }
-
-        //如果服务器连接不上，则抛出异常
-        if (session == null) {
-            throw new Exception("session is null");
-        }
-
-        //设置登陆主机的密码
-        session.setPassword(password);//设置密码
-        //设置第一次登陆的时候提示，可选值：(ask | yes | no)
-        session.setConfig("userauth.gssapi-with-mic","no");
-        session.setConfig("StrictHostKeyChecking", "no");
-        //设置登陆超时时间
-        session.connect(30000);
-
+    public void upload(byte[] bytes, String fileName) throws Exception{
         OutputStream outstream = null;
+        ChannelSftp sftp = pool.borrowObject();
         try {
-            //创建sftp通信通道
-            channel = (Channel) session.openChannel("sftp");
-            channel.connect(1000);
-            ChannelSftp sftp = (ChannelSftp) channel;
-
-
-            //进入服务器指定的文件夹
             sftp.cd(filepath);
-
-            //以下代码实现从本地上传一个文件到服务器，如果要实现下载，对换一下流就可以了
             outstream = sftp.put(fileName);
             outstream.write(bytes);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            //关流操作
+        }
+        finally {
             if (outstream != null) {
                 outstream.flush();
                 outstream.close();
             }
-            if (session != null) {
-                session.disconnect();
-            }
-            if (channel != null) {
-                channel.disconnect();
-            }
-            log.info("上传"+fileName+"成功!");
+            pool.returnObject(sftp);
         }
+    }
+
+    public void download(String localPath, String localName, String name) throws Exception{
+        InputStream in = getResource(name);
+        OutputStream out = new FileOutputStream(localPath+localName);
+        byte[] buff = new byte[1024 * 2];
+        int read;
+        if (in != null) {
+            do {
+                read = in.read(buff, 0, buff.length);
+                if (read > 0) {
+                    out.write(buff, 0, read);
+                }
+                out.flush();
+            } while (read >= 0);
+        }
+        if (in != null){
+            in.close();
+            out.close();
+        }
+    }
+
+    private InputStream getResource(String name){
+        ChannelSftp sftp = null;
+        try {
+            sftp = pool.borrowObject();
+            sftp.cd(filepath);
+            return sftp.get(name);
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            pool.returnObject(sftp);
+        }
+        return null;
+    }
+
+    public String getConfigInfo(){
+        return "MaxTotal:"+pool.myConfig.getMaxTotal()+";MaxIdle:"+pool.myConfig.getMaxIdle()+";MinIdle:"+pool.myConfig.getMinIdle();
     }
 }
--- art-web/src/main/java/com/art/artweb/controller/FileController.java
+++ art-web/src/main/java/com/art/artweb/controller/FileController.java
@@ -1,10 +1,13 @@
 package com.art.artweb.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import com.art.artcommon.constant.CustomException;
 import com.art.artcommon.entity.IResult;
-import com.art.artcommon.utils.FileUpLodeUtil;
+import com.art.artcommon.utils.FileUtils;
 import com.art.artcommon.utils.Tools;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -15,7 +18,7 @@ import org.springframework.web.multipart.MultipartFile;
 public class FileController {
 
     @Autowired
-    private FileUpLodeUtil fileUpLodeUtil;
+    private FileUtils fileUtils;
 
     /**
      * 文件上传
@@ -28,13 +31,31 @@ public class FileController {
         long timestamp = System.currentTimeMillis();
         String newFileName = Tools.getCode() + Tools.dateToStr(timestamp).substring(0,8) + fileName;
         try {
-            fileUpLodeUtil.upload(file.getBytes(),newFileName);
+            fileUtils.upload(file.getBytes(),newFileName);
         }catch (Exception e) {
             e.printStackTrace();
         }
         JSONObject object = new JSONObject();
         object.put("fileName",newFileName);
         return IResult.success(object);
+    }
+
+    @RequestMapping("/downloadFile")
+    public IResult downloadFile(@RequestBody JSONObject data) {
+        String localPath = data.getString("localPath");
+        String localName = data.getString("localName");
+        String name = data.getString("name");
+        try {
+            fileUtils.download(localPath,localName,name);
+        }catch (Exception e){
+            throw new CustomException("下载失败");
+        }
+        return IResult.success();
+    }
+
+    @PostMapping("/getupLoadFileConfigInfo")
+    public String getInfo(){
+        return fileUtils.getConfigInfo();
     }
 }
 
